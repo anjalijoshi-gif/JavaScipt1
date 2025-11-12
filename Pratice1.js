37de5c2449f0a953e7b504e10610903f023f83be
anjalijoshi-gif <81000450+anjalijoshi-gif@users.noreply.github.com>
2025-11-12 14:29:59 +0400

    Add files via upload

---
+++ Pratice1.js
@@ -0,0 +1,32 @@
+
+//1 Create a variable of type string and try to add a number on it//
+
+var a = "Anjali"
+var b = 20
+var c = a+b
+console.log(c)
+
+//2 use type of operator to find the datatype of string in last question
+
+console.log(typeof(c))
+
+//3 create a const object in javascript can you change it to hold a number later
+
+const a1 = {
+      nameee : "Ans",
+      place : "Dubai",
+      address : "Maza"
+}
+
+a1['nameee'] = " Nish "
+console.log(a1)
+
+//5 create a word-meaning dictionery of 2 words
+
+var dic = {
+     ugly : "not looking nice",
+     copy : "a thing made to be similar or identical to another",
+}
+
+console.log(dic['ugly'])
+
