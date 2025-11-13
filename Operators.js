3e6d95319c532a01a9a62d756b04ca7190b4e72c
anjalijoshi-gif <81000450+anjalijoshi-gif@users.noreply.github.com>
2025-11-13 11:32:26 +0400

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,50 @@
+
+console.log("Operators in js")
+// Arithmatic operators
+let a = 10;
+let b = 4;
+console.log(a+b)
+console.log(a-b)
+console.log(a/b)
+console.log(a**b)
+console.log(a%b)
+console.log(++a)
+console.log(a++)
+console.log(--a)
+console.log(a++)
+console.log(a)
+console.log(a--)
+
+console.log("Assignment operators")
+// Assignment Operators
+let assignment = 1 ;
+assignment += 5
+console.log(assignment)
+assignment -= 5
+console.log(assignment)
+assignment *= 5
+console.log(assignment)
+assignment /= 5
+console.log(assignment)
+
+// Comparison operators
+
+let comp1 = 6;
+let comp2 = 7;
+console.log(comp1 == comp2)
+
+console.log(comp1 != comp2)
+
+console.log(comp1 === comp2)
+console.log(comp1 > comp2)
+
+
+// Logical operators
+
+let x =5;
+let y = 6;
+console.log(x<< y && x ==5 )
+console.log(x>y || x == 5)
+
+console.log(!false)
+console.log(!true)
